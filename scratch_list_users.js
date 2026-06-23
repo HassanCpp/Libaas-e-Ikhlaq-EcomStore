b71bc65b6f0f4743a9404f25a57748a444ba6f11
Hassancpp <hassanwaqar475@gmail.com>
2026-06-23 15:38:53 +0500

    Fix file and directory case-sensitivity for Linux/Vercel compatibility

--- scratch_list_users.js
+++ scratch_list_users.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('./Models/User');
+const User = require('./models/User');
 
 const dbUri = 'mongodb://127.0.0.1:27017/libaseikhlaq';
 mongoose.connect(dbUri)
